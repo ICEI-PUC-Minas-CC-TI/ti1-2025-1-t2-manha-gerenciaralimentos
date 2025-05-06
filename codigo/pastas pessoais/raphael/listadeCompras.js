75470c9e2d67fe586e350808a389ea072c80b0c4
Raphael Lucas <raphael.paula@sga.pucminas.br>
2025-05-06 17:12:07 -0300

    Versão 2.0 da página da lista de compras

--- codigo/pastas pessoais/raphael/listadeCompras.js
+++ codigo/pastas pessoais/raphael/listadeCompras.js
@@ -4,73 +4,22 @@ const dados = {
         "id": 1,
         "nome": "banana",
         "tipo": "prata",
-        "imagem": "banana-prata.png",
+        "imagem": "assets/images/Banana_prata.png",
         "categoria": 1
       },
       {
         "id": 2,
         "nome": "batata",
         "tipo": "inglesa",
-        "imagem": "batata-inglesa.png",
+        "imagem": "assets/images/batata_inglesa.jpg",
         "categoria": 2
       },
       {
         "id": 3,
         "nome": "alface",
         "tipo": "americana",
-        "imagem": "alface-americana.png",
+        "imagem": "assets/images/Alface-Americana.png",
         "categoria": 3
-      }
-    ],
-
-    "ambientes": [
-      {
-        "id": 1,
-        "nome": "Geladeira",
-        "tipo": 1,
-        "imagem": "geladeira.png",
-        "itens": [
-          {
-            "alimentoId": 1,
-            "quantidade": 5,
-            "vencimento": "2025-02-12",
-            "cadastro": "2025-02-05"
-          },
-          {
-            "alimentoId": 3,
-            "quantidade": 2,
-            "vencimento": "2025-02-15",
-            "cadastro": "2025-02-10"
-          }
-        ]
-      },
-      {
-        "id": 2,
-        "nome": "Despensa",
-        "tipo": 2,
-        "imagem": "despensa.png",
-        "itens": [
-          {
-            "alimentoId": 2,
-            "quantidade": 10,
-            "vencimento": "2025-03-10",
-            "cadastro": "2025-02-01"
-          }
-        ]
-      },
-      {
-        "id": 3,
-        "nome": "Freezer",
-        "tipo": 1,
-        "imagem": "freezer.png",
-        "itens": [
-          {
-            "alimentoId": 2,
-            "quantidade": 4,
-            "vencimento": "2025-06-01",
-            "cadastro": "2025-01-15"
-          }
-        ]
       }
     ],
 
@@ -99,29 +48,8 @@ const dados = {
         ]
       }
     ],
-
-    "categoriaAlimento": [
-        { "id": 0, "categoria": "Outros" },
-        { "id": 1, "categoria": "Fruta" },
-        { "id": 2, "categoria": "Legume" },
-        { "id": 3, "categoria": "Vegetal" },
-        { "id": 4, "categoria": "Carne Bovina" },
-        { "id": 5, "categoria": "Carne Suína" },
-        { "id": 6, "categoria": "Aves" },
-        { "id": 7, "categoria": "Peixes e Frutos do Mar" },
-        { "id": 8, "categoria": "Soja e Derivados" },
-        { "id": 9, "categoria": "Laticínio" },
-        { "id": 10, "categoria": "Grãos e Cereais" }
-      ],
-
-    "tipoAmbiente": [
-        { "id": 0, "tipo": "Outros" },
-        { "id": 1, "tipo": "Refrigeração" },
-        { "id": 2, "tipo": "Seco" },
-        { "id": 3, "tipo": "Congelado" }
-    ]
 }
-  
+
 const container = document.getElementById('listadeCompras');
 
 dados.listasDeCompra.forEach(lista => {
@@ -168,4 +96,4 @@ dados.listasDeCompra.forEach(lista => {
     });
 
     container.appendChild(listaSection);
-});
+})
