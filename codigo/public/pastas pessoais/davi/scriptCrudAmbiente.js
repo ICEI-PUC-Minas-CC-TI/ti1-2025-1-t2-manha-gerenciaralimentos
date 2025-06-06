9ac508e09ab0ff75905166c8ef5e1bf0c9f666e3
Davi Martins <davi.gurgel@sga.pucminas.br>
2025-06-06 17:50:20 -0300

    Parte inicial do JAVASCRIPT CRUD de ambientes

---
+++ codigo/public/pastas pessoais/davi/scriptCrudAmbiente.js
@@ -0,0 +1,11 @@
+const apiUrl = 'https://json-server-stockit.onrender.com/ambientes';
+
+// Elementos do DOM
+const nomeInput = document.getElementById('nomeAmbiente');
+const tipoInput = document.getElementById('tipoAmbiente');
+const imagemInput = document.getElementById('imagem-ambiente');
+
+const btnConfirmar = document.getElementById('btnConfirmar');
+const btnCancelar = document.getElementById('btnCancelar');
+const btnAtualizar = document.getElementById('btnAtualizar');
+const btnExcluir = document.getElementById('btnExcluir');
